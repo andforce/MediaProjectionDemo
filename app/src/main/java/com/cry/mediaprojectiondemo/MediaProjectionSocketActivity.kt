161d478e285f1d732f2d38680d9d747a6a105a1e
diyuan <wangdiyuan@email.com>
2024-03-15 18:37:43 +0800

    start activity

--- app/src/main/java/com/cry/mediaprojectiondemo/MediaProjectionSocketActivity.kt
+++ app/src/main/java/com/cry/mediaprojectiondemo/MediaProjectionSocketActivity.kt
@@ -8,15 +8,18 @@ import android.os.Bundle
 import android.util.DisplayMetrics
 import android.view.WindowManager
 import android.widget.Button
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import com.cry.mediaprojectiondemo.socket.SocketIoManager
 import com.cry.screenop.RecorderHelper
 import java.io.ByteArrayOutputStream
 
+
 class MediaProjectionSocketActivity : AppCompatActivity() {
 
     var mpm: MediaProjectionManager? = null
-    val code = 1
 
     private val MAX_IMAGE_HEIGHT = 480
 
@@ -30,52 +33,55 @@ class MediaProjectionSocketActivity : AppCompatActivity() {
             SocketIoManager.connect()
         }.start()
 
+        val launcher: ActivityResultLauncher<Intent> = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()
+        ) { result: ActivityResult ->
+            if (result.resultCode == RESULT_OK) {
+                val data: Intent? = result.data
+                data?.let {
+                    val mp = mpm?.getMediaProjection(
+                        result.resultCode, data
+                    )
+
+                    mp?.let {
+
+                        val metrics = DisplayMetrics()
+                        val windowMgr = getSystemService(Context.WINDOW_SERVICE) as WindowManager
+                        windowMgr.defaultDisplay.getRealMetrics(metrics)
+
+                        // 获取屏幕宽高
+                        var widthPixels = metrics.widthPixels.toFloat()
+                        var heightPixels = metrics.heightPixels.toFloat()
+                        if (heightPixels > MAX_IMAGE_HEIGHT) {
+                            // heightPixels        MAX_IMAGE_HEIGHT
+                            // ------------  =     ----------------
+                            // widthPixels         x
+                            widthPixels = widthPixels * MAX_IMAGE_HEIGHT.toFloat() / heightPixels
+                            heightPixels = MAX_IMAGE_HEIGHT.toFloat()
+                        }
+                        val finalWidthPixels = widthPixels.toInt()
+                        val finalHeightPixels = heightPixels.toInt()
+
+                        RecorderHelper.startRecording(windowMgr,finalWidthPixels, finalHeightPixels, mp) {
+                            it?.let {
+                                sendBitmap(it)
+                            }
+                        }
+                    }
+
+                }
+            }
+        }
+
         findViewById<Button>(R.id.btn_start).setOnClickListener {
 
             val intent = mpm?.createScreenCaptureIntent()
             intent?.let {
-                startActivityForResult(intent, code)
+                launcher.launch(intent)
             }
         }
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == code && resultCode == RESULT_OK) {
-            data?.let {
-                val mp = mpm?.getMediaProjection(
-                    resultCode, data
-                )
-
-                mp?.let {
-
-                    val metrics = DisplayMetrics()
-                    val windowMgr = getSystemService(Context.WINDOW_SERVICE) as WindowManager
-                    windowMgr.defaultDisplay.getRealMetrics(metrics)
-
-                    // 获取屏幕宽高
-                    var widthPixels = metrics.widthPixels.toFloat()
-                    var heightPixels = metrics.heightPixels.toFloat()
-                    if (heightPixels > MAX_IMAGE_HEIGHT) {
-                        // heightPixels        MAX_IMAGE_HEIGHT
-                        // ------------  =     ----------------
-                        // widthPixels         x
-                        widthPixels = widthPixels * MAX_IMAGE_HEIGHT.toFloat() / heightPixels
-                        heightPixels = MAX_IMAGE_HEIGHT.toFloat()
-                    }
-                    val finalWidthPixels = widthPixels.toInt()
-                    val finalHeightPixels = heightPixels.toInt()
-
-                    RecorderHelper.startRecording(windowMgr,finalWidthPixels, finalHeightPixels, mp) {
-                        it?.let {
-                            sendBitmap(it)
-                        }
-                    }
-                }
-
-            }
-        }
-    }
 
     //bitmap to byteArray to send through socket
     private fun sendBitmap(it: Bitmap) {
