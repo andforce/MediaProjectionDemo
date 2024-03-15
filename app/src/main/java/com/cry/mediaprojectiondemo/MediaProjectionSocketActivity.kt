45562cf4fa2f3b71f479f5059a14bed7eebfd97b
diyuan <wangdiyuan@email.com>
2024-03-15 17:15:50 +0800

    refactor

--- app/src/main/java/com/cry/mediaprojectiondemo/MediaProjectionSocketActivity.kt
+++ app/src/main/java/com/cry/mediaprojectiondemo/MediaProjectionSocketActivity.kt
@@ -2,33 +2,18 @@ package com.cry.mediaprojectiondemo
 
 import android.graphics.Bitmap
 import android.os.Bundle
-import android.os.Handler
-import android.os.Message
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import com.cry.mediaprojectiondemo.socket.ServerThread
 import com.cry.mediaprojectiondemo.socket.SocketIoManager
 import com.cry.screenop.RxScreenShot
 import java.io.ByteArrayOutputStream
 
 class MediaProjectionSocketActivity : AppCompatActivity() {
     private var isBack: Boolean = false
-    private var serverThread: ServerThread? = null
-    private var serverHandler: Handler? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        //start server thread
-        serverThread = ServerThread()
-        serverThread!!.start()
-        serverHandler = object : Handler(serverThread!!.looper) {
-            override fun handleMessage(msg: Message) {
-                super.handleMessage(msg)
-                sendBitmap(msg.obj as Bitmap)
-            }
-        }
 
         //connect to socket server
         Thread {
@@ -45,9 +30,7 @@ class MediaProjectionSocketActivity : AppCompatActivity() {
                                 moveTaskToBack(true)
                                 isBack = true
                             } else {
-                                val obtainMessage = serverHandler!!.obtainMessage()
-                                obtainMessage.obj = bitmap
-                                serverHandler!!.sendMessage(obtainMessage)
+                                sendBitmap(bitmap)
                             }
                         }
 
@@ -66,6 +49,5 @@ class MediaProjectionSocketActivity : AppCompatActivity() {
     override fun onDestroy() {
         super.onDestroy()
         SocketIoManager.release()
-        serverThread!!.quitSafely()
     }
 }
--- app/src/main/java/com/cry/mediaprojectiondemo/socket/ServerThread.kt
+++
@@ -1,8 +0,0 @@
-package com.cry.mediaprojectiondemo.socket
-
-import android.os.HandlerThread
-
-/**
- * Created by a2957 on 4/23/2018.
- */
-class ServerThread : HandlerThread("ServerThread")
